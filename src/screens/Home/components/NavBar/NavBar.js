b5cb77e9deb8bfdd2598c152b415b02f3af2135d
faruch ismailov <58651410+farucis@users.noreply.github.com>
2023-03-27 03:35:08 +0300

    update navbar ui/ux

--- src/screens/Home/components/NavBar/NavBar.js
+++ src/screens/Home/components/NavBar/NavBar.js
@@ -1,35 +1,58 @@
 import React from "react";
 import "./NavBar.css";
 
-import NabBarMenu from "./NabBarMenu";
-
-import { GiHamburgerMenu } from "react-icons/gi";
-import { GrClose } from "react-icons/gr";
 import { FaWaze, FaInstagramSquare, FaPhoneSquare } from "react-icons/fa";
 
-const NavBar = () => {
-  const [isOpen, setIsOpen] = React.useState(false);
-
+const Test = () => {
+  const items = [
+    "?מי אני",
+    "קצת על הסטודיו",
+    "תגובות של לקוחות",
+    "חבילות אימונים",
+    "צור קשר",
+  ];
   return (
-    <div className="navBar-container">
+    <div>
+      <div class="header">
       <div className="n-logo"></div>
       <div className="contact-buttones">
-        <a href="https://ul.waze.com/ul?preview_venue_id=22806840.228002868.613111&navigate=yes&utm_campaign=default&utm_source=waze_website&utm_medium=lm_share_location">
+        <a target="_blank" rel="noreferrer" href="https://ul.waze.com/ul?preview_venue_id=22806840.228002868.613111&navigate=yes&utm_campaign=default&utm_source=waze_website&utm_medium=lm_share_location">
           <FaWaze />
         </a>
-        <a href="https://www.instagram.com/invites/contact/?i=x7ikcv5pitc5&utm_content=uf68p%E2%80%8E%E2%80%8F">
+        <a target="_blank" rel="noreferrer" href="https://www.instagram.com/invites/contact/?i=x7ikcv5pitc5&utm_content=uf68p%E2%80%8E%E2%80%8F">
           <FaInstagramSquare />
         </a>
-        <a href="https://wa.me/message/EZX2HIMY3WDRM1">
+        <a target="_blank" rel="noreferrer" href="https://wa.me/message/EZX2HIMY3WDRM1">
           <FaPhoneSquare />
         </a>
       </div>
-      <i onClick={() => setIsOpen(!isOpen)}>
-        {!isOpen ? <GiHamburgerMenu /> : <GrClose />}
-      </i>
-      {isOpen && <NabBarMenu />}
+      </div>
+      <input type="checkbox" class="openSidebarMenu" id="openSidebarMenu" />
+      <label for="openSidebarMenu" class="sidebarIconToggle">
+        <div class="spinner diagonal part-1"></div>
+        <div class="spinner horizontal"></div>
+        <div class="spinner diagonal part-2"></div>
+      </label>
+      <label className="overlyy" ></label>
+
+      <div id="sidebarMenu">
+        <ul class="sidebarMenuInner">
+          <li>
+            שרון ליפוביץ אבוקסיס<span>מאמנת כושר</span>
+          </li>
+          {items.map((item, index) => {
+            return (
+              <li>
+                <a href="" target="_blank" rel="noreferrer">
+                  {item}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 };
 
-export default NavBar;
+export default Test;
