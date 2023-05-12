500fc51529176dce41f401a8efbdf8daf48b079d
faruch ismailov <58651410+farucis@users.noreply.github.com>
2023-05-13 01:58:26 +0300

    updatet navbar UI/UX

--- src/assets/pictures/UI_MAGES.js
+++ src/assets/pictures/UI_MAGES.js
@@ -2,6 +2,7 @@ const image1 = require("./logo/first.png");
 const image2 = require("./logo/second.png");
 const image3 = require("./logo/לוגו גדלים-02.png");
 const image4 = require("./logo/background-lines.png");
+const image5 = require("./logo/text-logo.png");
 
-const UI_MAGES = [image1, image2, image3, image4];
+const UI_MAGES = [image1, image2, image3, image4, image4,image5];
 export default UI_MAGES;
