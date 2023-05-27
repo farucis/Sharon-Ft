2b4ef0f929fad92bcce238c357c73c8943bf1368
faruch ismailov <58651410+farucis@users.noreply.github.com>
2023-05-27 14:40:03 +0300

    update UI

--- src/screens/TrainingPackages/components/TrainLevales/TrainLevales.js
+++ src/screens/TrainingPackages/components/TrainLevales/TrainLevales.js
@@ -4,32 +4,41 @@ import "./TrainLevales.css";
 const TrainLevales = () => {
   return (
     <ul className="tilesWrap">
-      <li>
-        <h2>01</h2>
-        <h3>אימון קבוצתי בודד</h3>
-        <p>בעלות של 50 ש"ח בתשלום מראש ללא התחייבות</p>
-        <button>קרא עוד</button>
-      </li>
-      <li>
-        <h2>02</h2>
-        <h3>basic מסלול חודשי</h3>
-        <p>בעלות של 349 ש"ח ללא התחייבות ללא דמי ביטול</p>
-        <button>קרא עוד</button>
-      </li>
-      <li>
-        <h2>03</h2>
-        <h3>premium מסלול חודשי </h3>
-        <p>בעלות של 449 ש"ח ללא התחייבות ללא דמי ביטול</p>
-        <button>קרא עוד</button>
-      </li>
-      <li>
-        <h2>04</h2>
-        <h3>אימון אישי 1 על 1 </h3>
-        <p>בעלות של 150 ש"ח בתשלום מראש ללא התחייבות</p>
-        <button>קרא עוד</button>
-      </li>
+      {trainLevalesDB.map((obj) => {
+        return (
+          <li>
+            <h2>0{obj.id}</h2>
+            <h3>{obj.title}</h3>
+            <p>{obj.info}</p>
+            <button>קרא עוד</button>
+          </li>
+        );
+      })}
     </ul>
   );
 };
 
 export default TrainLevales;
+
+const trainLevalesDB = [
+  {
+    id: 1,
+    title: "אימון קבוצתי בודד",
+    info: 'בעלות של 50 ש"ח בתשלום מראש ללא התחייבות',
+  },
+  {
+    id: 2,
+    title: "basic מסלול חודשי",
+    info: 'בעלות של 349 ש"ח ללא התחייבות ללא דמי ביטול',
+  },
+  {
+    id: 3,
+    title: "premium מסלול חודשי",
+    info: 'בעלות של 449 ש"ח ללא התחייבות ללא דמי ביטול',
+  },
+  {
+    id: 4,
+    title: "אימון אישי 1 על 1",
+    info: 'בעלות של 150 ש"ח בתשלום מראש ללא התחייבות',
+  },
+];
